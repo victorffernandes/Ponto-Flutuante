8d3c78b36414e46eecf5d3bcec657acba3f5841d
mauriciolfsilva <mauriciolfsilva@yahoo.com.br>
2017-10-29 18:12:57 -0200

    IEEE Working

--- mian.py
+++ mian.py
@@ -1,13 +1,4 @@
-num = input("Insira o número na base 2 a ser transformado para ponto flutuante")
-result = ""
-if num == "0" or str == "+0" or str == "-0":
-    result = "0 00000000 00000000000000000000000"
-elif num == "-inf":
-    result = "1 11111111 00000000000000000000000"
-elif num == "+inf":
-    result = "0 11111111 00000000000000000000000"
-
-
+num = input("Insira o número na base 2 a ser transformado para ponto flutuante: ")
 
 def binToDec(a):
     result = 0
@@ -21,7 +12,7 @@ def binToDec(a):
     for i in range(len(algarismos)):
         result += float(algarismos[(len(algarismos) - 1) - i]) * 2**i
 
-    return result * sign
+    return result #* sign
 
 def decToBin(b):
     sign = ""
@@ -39,10 +30,49 @@ def decToBin(b):
 
     return sign + ((i + "1")[::-1])
 
-#Excesso em K - bin, bin
 def excess (num, k):
     n1 = binToDec(num)
-    n2 = binToDec(k)
 
-    return decToBin(int(n1 -  n2))
-print(excess("1101010","1111111"))
+    return decToBin(int(n1 + k))
+
+def getIEEE(num):
+
+    if num == "0" or str == "+0" or str == "-0":
+        return "0 00000000 00000000000000000000000"
+    elif num == "-inf":
+        return "1 11111111 00000000000000000000000"
+    elif num == "+inf":
+        return "0 11111111 00000000000000000000000"
+
+    try:
+        t = int(num[1:])
+    except:
+        return "0 11111111 00010000000000000000001"
+
+    if num[1] == 1:
+        n1 = num.split(",")[0]
+        n2 = ""
+
+        for i in range(2, len(n1)):
+            n2 += n1[i]
+
+        n2 += num.split(",")[1]
+
+        for i in range(len(n2), 23):
+            n2 += "0"
+
+        if num[0] == "-":
+            return "1 " + excess("+" + len(n1) - 2, 127) + " " + n2
+        else:
+            return "0 " + excess("+" + str(len(n1) - 2), 127) + " " + n2
+
+    else:
+
+        n2 = num[len(num)-24:]
+
+        if num[0] == "-":
+            return "1 " + "00000000 " + n2
+        else:
+            return "0 " + "00000000 " + n2
+
+print(getIEEE("num"))
